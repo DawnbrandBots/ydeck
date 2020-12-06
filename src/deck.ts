5d9211b306336129956bd180637aff1fbb1e36e2
AlphaKretin <deewhydeeecks@gmail.com>
2020-12-06 15:00:39 +1030

    Push again to trigger CI

--- src/deck.ts
+++ src/deck.ts
@@ -6,7 +6,7 @@ export class Deck {
 		const urls = extractURLs(url);
 		if (urls.length < 1) {
 			throw new Error(
-				"Decks must be initialised with a YDKE URL! If you have another format, use one of the converter functions!"
+				"Decks must be initialised with a YDKE URL!" // If you have another format, use one of the converter functions!"
 			);
 		}
 		this.url = url;
