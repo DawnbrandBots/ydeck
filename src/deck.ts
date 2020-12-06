089823c42443e3c93e06c17b0ce123dc4d592fdd
AlphaKretin <deewhydeeecks@gmail.com>
2020-12-06 17:26:28 +1030

    Add helpers to construct with YDK or TypedDeck

--- src/deck.ts
+++ src/deck.ts
@@ -1,4 +1,5 @@
-import { extractURLs } from "ydke";
+import { TypedDeck, extractURLs, toURL } from "ydke";
+export { TypedDeck };
 
 export class Deck {
 	private url: string;
@@ -6,9 +7,44 @@ export class Deck {
 		const urls = extractURLs(url);
 		if (urls.length < 1) {
 			throw new Error(
-				"Decks must be initialised with a YDKE URL!" // If you have another format, use one of the converter functions!"
+				"Decks must be initialised with a YDKE URL! If you have another format, use one of the converter functions!"
 			);
 		}
 		this.url = url;
+	}
+
+	public static TypedDeckToUrl(deck: TypedDeck): string {
+		return toURL(deck);
+	}
+
+	public static YdkToUrl(ydk: string): string {
+		const deck = ydk.split(/\r|\n|\r\n/);
+		const mainIndex = deck.indexOf("#main");
+		if (mainIndex < 0) {
+			throw new Error("YDK input does not conform to expected format!");
+		}
+		const extraIndex = deck.indexOf("#extra");
+		if (extraIndex < 0) {
+			throw new Error("YDK input does not conform to expected format!");
+		}
+		const sideIndex = deck.indexOf("!side");
+		if (sideIndex < 0) {
+			throw new Error("YDK input does not conform to expected format!");
+		}
+
+		const mainString = deck.slice(mainIndex + 1, extraIndex - 1);
+		const mainNumber = mainString.map(s => parseInt(s, 10));
+		const main = Uint32Array.from(mainNumber);
+
+		const extraString = deck.slice(extraIndex + 1, sideIndex - 1);
+		const extraNumber = extraString.map(s => parseInt(s, 10));
+		const extra = Uint32Array.from(extraNumber);
+
+		const sideString = deck.slice(sideIndex + 1);
+		const sideNumber = sideString.map(s => parseInt(s, 10));
+		const side = Uint32Array.from(sideNumber);
+
+		const typedDeck = { main, extra, side };
+		return Deck.TypedDeckToUrl(typedDeck);
 	}
 }
--- src/test/test.ts
+++ src/test/test.ts
@@ -5,14 +5,23 @@ import { Deck } from "../deck";
 const url =
 	"ydke://5m3qBeZt6gV9+McCffjHAn34xwK8beUDvG3lA7xt5QMfX5ICWvTJAVr0yQFa9MkBrDOdBKwznQSsM50Ey/UzAMv1MwDL9TMAdAxQBQ6wYAKvI94AryPeAK8j3gCmm/QBWXtjBOMavwDjGr8A4xq/AD6kcQE+pHEBPqRxAZ4TygSeE8oEnhPKBKUt9QOlLfUDpS31AyJImQAiSJkAIkiZAA==!FtIXALVcnwC1XJ8AiBF2A4gRdgNLTV4Elt0IAMf4TQHCT0EAvw5JAqSaKwD5UX8EweoDA2LO9ATaI+sD!H1+SAg==!";
 
-const badUrl =
+const ydk =
+	"#created by AlphaKretin\n#main\n99249638\n99249638\n46659709\n46659709\n46659709\n65367484\n65367484\n65367484\n43147039\n30012506\n30012506\n30012506\n77411244\n77411244\n77411244\n3405259\n3405259\n3405259\n89132148\n39890958\n14558127\n14558127\n14558127\n32807846\n73628505\n12524259\n12524259\n12524259\n24224830\n24224830\n24224830\n80352158\n80352158\n80352158\n66399653\n66399653\n66399653\n10045474\n10045474\n10045474\n#extra\n1561110\n10443957\n10443957\n58069384\n58069384\n73289035\n581014\n21887175\n4280258\n38342335\n2857636\n75452921\n50588353\n83152482\n65741786\n!side\n43147039\n";
+
+const badString =
 	"Cuz we're gonna shout it loud, even if our words seem meaningless, like we're carrying the weight of the world";
 
-describe("Basic test", function () {
-	it("Successful construction", function () {
+describe("Construction", function () {
+	it("Successful construction with URL", function () {
 		expect(() => new Deck(url)).to.not.throw();
 	});
-	it("Failed construction", function () {
-		expect(() => new Deck(badUrl)).to.throw();
+	it("Failed construction with URL", function () {
+		expect(() => new Deck(badString)).to.throw();
+	});
+	it("Successful construction with YDK", function () {
+		expect(() => new Deck(Deck.YdkToUrl(ydk))).to.not.throw();
+	});
+	it("Failed construction with YDK", function () {
+		expect(() => new Deck(Deck.YdkToUrl(badString))).to.throw();
 	});
 });
