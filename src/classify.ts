581ece70413cd0a81559fd10f75cb175aa12e784
AlphaKretin <deewhydeeecks@gmail.com>
2020-12-16 14:04:39 +1030

    Add classifiers for currently "meta" decks

--- src/classify.ts
+++ src/classify.ts
@@ -43,17 +43,86 @@ export function classify(deck: TypedDeck, vector: CardVector, data: CardArray): 
 	return themes;
 }
 
-const blackwing = setCodeClassifier(0x33, 10, 3);
 const mekkKnight = setCodeClassifier(0x10c, 5);
-const mysticMineReqs = {
+
+const mysticMine = cardCodeClassifier({
 	76375976: 3, // Mystic Mine
 	7153114: 1 // Field Barrier
+});
+
+const virtualWorld = setCodeClassifier(0x150, 5);
+
+const drytron = setCodeClassifier(0x151, 5);
+
+const infernoble = (deck: TypedDeck, vec: CardVector, data: CardArray) => {
+	// noble knight + charles
+	return setCodeClassifier(0x107a, 5)(deck, vec, data) && cardCodeClassifier({ 77656797: 1 })(deck, vec, data);
 };
 
-const mysticMine = cardCodeClassifier(mysticMineReqs);
+const dlink = cardCodeClassifier({
+	11969228: 1, // Romulus
+	15381421: 2 // Seyfert
+});
+
+const dino = cardCodeClassifier({
+	44335251: 3, // Ovi
+	18940556: 1 // UCT
+});
+
+const eldlich = (deck: TypedDeck, vec: CardVector, data: CardArray) => {
+	// eldlich + eldlixir + golden land
+	return (
+		setCodeClassifier(0x142, 3)(deck, vec, data) &&
+		setCodeClassifier(0x143, 3)(deck, vec, data) &&
+		setCodeClassifier(0x1434, 3)(deck, vec, data)
+	);
+};
+
+const zoodiac = setCodeClassifier(0xf1, 5);
+
+const invoked = (deck: TypedDeck, vec: CardVector, data: CardArray) => {
+	// invoked + aleister + invoc
+	return (
+		setCodeClassifier(0xf4, 1)(deck, vec, data) &&
+		cardCodeClassifier({
+			86120751: 1,
+			74063034: 1
+		})(deck, vec, data)
+	);
+};
+
+const dogma = setCodeClassifier(0x146, 5);
+
+const pk = cardCodeClassifier({ 25538345: 3 }); // Torn Scales
+
+const pranks = setCodeClassifier(0x120, 5);
+
+const blader = cardCodeClassifier({
+	11790356: 1, // Buster Dragon
+	86240887: 1, // The fusion
+	32104431: 1, // Fusion trap
+	33280639: 1 // Synchro trap
+});
+
+const striker = setCodeClassifier(0x115, 5);
+
+const geist = setCodeClassifier(0x103, 5);
 
 const classifiers: { [theme: string]: Classifier } = {
-	Blackwing: blackwing,
 	"Mekk-Knight": mekkKnight,
-	"Mystic Mine": mysticMine
+	"Mystic Mine": mysticMine,
+	"Virtual World": virtualWorld,
+	Drytron: drytron,
+	"Infernoble Knight": infernoble,
+	"Dragon Link": dlink,
+	Dinosaur: dino,
+	Eldlich: eldlich,
+	Zoodiac: zoodiac,
+	Invoked: invoked,
+	Dogmatika: dogma,
+	"Phantom Knight": pk,
+	"Prank-Kids": pranks,
+	"Buster Blader": blader,
+	"Sky Striker": striker,
+	Altergeist: geist
 };
